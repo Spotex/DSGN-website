e62aa3cf72fba984a20e684e19b02db4f707cb34
Sebastian Potekhin <seva.potekhin@icloud.com>
2017-12-12 14:23:06 +0200

    Add files via upload

--- js/hover.js
+++ js/hover.js
@@ -1,6 +1,23 @@
 $(function (){ 
-    $('.list a:nth-child(#)').onmouseover(function(){
-       var html = $('.list a:nth-child(#)').text();
-       alert(html);
-    });
+	$('body').click(function (event) {
+	    t=event.target||event.srcElement;
+	    $(t).css('fill','red');
+	    $(t).css('stroke','red');
+	    $(t).closest('svg').css('transform','scale(1.1)');
+	    $(t).closest('svg').css('cursor','default');
+	    $(t).closest('svg').siblings('span').animate({
+	       	opacity: "1"
+	    }, 800);
+	})
 });
+/*$(function (){ 
+    $('.i_heart path').click(function(){
+       $('.i_heart path').css('fill','red');
+       $('.i_heart path').css('stroke','red');
+       $('.i_heart svg').css('transform','scale(1.1)');
+       $('.i_heart svg').css('cursor','default');
+       $('.i_heart span').animate({
+       		opacity: "1"
+       }, 800);
+    });
+});*/
